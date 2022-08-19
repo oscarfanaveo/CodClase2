ad9c2b3a9a0d92f0536c242436e7e7498748b201
oscarfanaveo <87046213+oscarfanaveo@users.noreply.github.com>
2022-08-19 11:23:35 -0400

    Update and rename Pizza.java to PC.java

---
+++ src/clases/PC.java
@@ -0,0 +1,34 @@
+package clases;
+
+// CLASE PRODUCTO.
+public class PC {
+    private String cpu = "";
+    private String gpu = "";
+    private String ram = "";
+    private String disco = "";
+    private String motherboard = "";
+    private String case = "";
+ 
+    public void setCpu(String masa) { 
+        this.cpu = cpu; 
+    }
+    
+    public void setGpu(String salsa) { 
+        this.gpu = gpu; 
+    }
+    
+    public void setRam(String relleno) { 
+        this.ram = ram; 
+    }
+    public void setDisco(String masa) { 
+        this.disco = disco; 
+    }
+    
+    public void setMotherboard(String salsa) { 
+        this.motherboard = motherboard; 
+    }
+    
+    public void setCase(String relleno) { 
+        this.case = case; 
+    }
+}
